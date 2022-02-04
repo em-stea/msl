f41de14f72849b0317286acbddbb4c6aee404029
em-stea <emiliastea@hotmail.com>
2022-02-04 17:44:31 -0300

    upd

--- form-sumate.php
+++ form-sumate.php
@@ -8,7 +8,7 @@ $db = new PDO($db_dsn, $db_user, $db_pass, $db_options);
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $email = $_POST['email'];
-$email = $_POST['telefono'];
+$telefono = $_POST['telefono'];
 
 /*guardo los datos en la db*/
 $sql = 'insert into sumate (nombre,apellido, email, telefono) values (?, ?)'; //nombres de las columnas de la db
